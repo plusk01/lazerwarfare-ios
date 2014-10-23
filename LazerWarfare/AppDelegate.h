8bdbf411cba38d58e9963f36a5e3aea8502285ab
Parker Lusk <parkerclusk@gmail.com>
2014-10-22 19:25:58 -0600

    Initial Commit

---
+++ LazerWarfare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LazerWarfare
+//
+//  Created by Parker Lusk on 10/22/14.
+//  Copyright (c) 2014 Parker Lusk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
