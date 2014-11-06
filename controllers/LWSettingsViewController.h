6027a1127d9a7c5ef90ff4c4f7f482b04a900c33
Parker Lusk <parkerclusk@gmail.com>
2014-11-05 23:17:37 -0700

    Added ViewControllers for the different views that branch off the main screen

---
+++ controllers/LWSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LWSettingsViewController.h
+//  LazerWarfare
+//
+//  Created by Parker Lusk on 11/5/14.
+//  Copyright (c) 2014 Parker Lusk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWSettingsViewController : UIViewController
+
+@end
